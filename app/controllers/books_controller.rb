44afb11c1c3ef7eb79fc1644ba15d33c30fae517
codecademy <ccuser@codecademy.com>
2016-05-17 06:40:09 -0400

    Finished Project

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,16 @@
+class BooksController < ApplicationController
+  
+  def index
+    @books=Book.all
+  end
+  
+  
+  def show 
+    @book = Book.find(params[:id]) 
+    @reviews = @book.reviews 
+  end
+  
+  
+
+
+end
